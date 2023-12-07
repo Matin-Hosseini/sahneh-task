31131d326fc3924e603a772ca2c205f24caacbed
Matin Hosseini <matinhosseiniofficial@gmail.com>
2023-12-07 12:53:10 +0330

    security ui for desktop completed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-const { createApp, reactive , ref} = Vue;
+const { createApp, reactive, ref } = Vue;
 
 createApp({
   setup() {
     const message = ref("matin");
+    const phoneNumber = "0912***541";
+
+    const activeTab = ref("enter");
+
+    const setActive = (tab) => {
+      activeTab.value = tab;
+    };
+
+    const isActive = (tab) => {
+      return activeTab.value === tab;
+    };
 
     return {
       message,
+      phoneNumber,
+      activeTab,
+      setActive,
+      isActive,
     };
   },
 }).mount("#app");
-
